9199042b0b6baf51a5be0460f402f9d512a4981e
Harprit Singh <harp.singh@gmail.com>
2022-03-04 13:27:21 +0530

    Base commit

---
+++ football_schedule.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://www.goal.com/en-in/fixtures/"
+
+
+def get_schedule(date):
+    """Provide date in YYYY-MM-DD format as a string."""
+
+    response = requests.get(URL + date)
+    response.raise_for_status()
+    goal_text = response.text
+
+    soup = BeautifulSoup(goal_text, "html.parser")
+
+    all_competitions = soup.find_all(name="div", class_="competition-matches")
+
+    if all_competitions:
+        schedule_list = []
+        scheduled_matches = False
+        for competition in all_competitions:
+            competition_name = competition.select_one("div.competition-wrapper a span").get_text()
+            all_matches = competition.select("div.match-row__data")
+
+            if all_matches:
+                scheduled_matches = True
+                matches_list = []
+                for match in all_matches:
+                    match_time = match.select_one("div.match-row__status span").get_text()
+                    home_team = match.select_one("table.match-row__teams tr td.match-row__team-home a span").get_text()
+                    away_team = match.select_one("table.match-row__teams tr td.match-row__team-away a span").get_text()
+                    matches_list.append(
+                        {
+                            "time": match_time,
+                            "home_team": home_team,
+                            "away_team": away_team,
+                        }
+                    )
+
+                schedule_list.append(
+                    {
+                        "competition": competition_name,
+                        "matches": matches_list
+                    }
+                )
+
+        if scheduled_matches:
+            return schedule_list
+        else:
+            return "No matches scheduled on that day"
---
+++ main.py
@@ -0,0 +1,5 @@
+from football_schedule import get_schedule
+from pprint import pprint
+
+schedule = get_schedule("2022-03-05")
+pprint(schedule)
